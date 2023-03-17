5bd3fa71743fafef3499a4528fcbf42d9d43fa59
Tom Sewell <122285429+tsewell93@users.noreply.github.com>
2023-03-17 14:51:10 +0000

    Added add track to playlist functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,15 +5,37 @@ import {SearchResults} from '../../Components/SearchResults/SearchResults';
 import {Playlist} from '../../Components/Playlist/Playlist';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [{name: "Song Name 1", artist: 'Artist Name 1', album: 'Album Name 1', id: 'Song ID 1'},
+      {name: "Song Name 2", artist: 'Artist Name 2', album: 'Album Name 2', id: 'Song ID 2'},
+      {name: "Song Name 3", artist: 'Artist Name 3', album: 'Album Name 3', id: 'Song ID 3'}],
+      playlistName: 'Playlist Name',
+      playlistTracks: [{name: "PL Song Name 1", artist: 'PL Artist Name 1', album: 'PL Album Name 1', id: 'PL Song ID 1'},
+      {name: "PL Song Name 2", artist: 'PL Artist Name 2', album: 'PL Album Name 2', id: 'PL Song ID 2'},
+      {name: "PL Song Name 3", artist: 'PL Artist Name 3', album: 'PL Album Name 3', id: 'PL Song ID 3'}]
+  };
+    this.addTrack = this.addTrack.bind(this);
+  }
+  addTrack(track){
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)){
+      return;
+    } else {
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+    }
+  }
   render(){
     return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          {/* Add a SearchBar component */}
+          <SearchBar />
           <div className="App-playlist">
-            {/* Add a SearchResults component */}
-            {/* Add a Playlist component */}
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
     </div>
