5de8728b10d59c840d8881c16d4b3a03da73bb2b
Tom Sewell <122285429+tsewell93@users.noreply.github.com>
2023-03-18 11:02:52 +0000

    Added functionality to app to change name of saved playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -18,6 +18,7 @@ class App extends React.Component{
   };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
   addTrack(track){
     let tracks = this.state.playlistTracks;
@@ -35,6 +36,10 @@ class App extends React.Component{
     this.setState({playlistTracks: tracks});
   }
 
+  updatePlaylistName(name){
+    this.setState({playlistName: name});
+  }
+
   render(){
     return (
     <div>
@@ -45,7 +50,8 @@ class App extends React.Component{
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks}
-                      onRemove={this.removeTrack}/>
+                      onRemove={this.removeTrack}
+                      onNameChange={this.updatePlaylistName}/>
           </div>
         </div>
     </div>
