3de48888996f87a7ab6619b145d24984547ccdd4
Tom Sewell <122285429+tsewell93@users.noreply.github.com>
2023-03-18 11:46:17 +0000

    Added search functionality for the search bar component (not yet linked to Spotify API

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,7 +19,10 @@ class App extends React.Component{
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
+
   addTrack(track){
     let tracks = this.state.playlistTracks;
     if (tracks.find(savedTrack => savedTrack.id === track.id)){
@@ -40,18 +43,27 @@ class App extends React.Component{
     this.setState({playlistName: name});
   }
 
+  savePlaylist(){
+    const trackURIs = this.state.playlistTracks.map(track => track.URI);
+  }
+
+  search(searchTerm){
+    console.log(searchTerm);
+  }
+
   render(){
     return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks}
                       onRemove={this.removeTrack}
-                      onNameChange={this.updatePlaylistName}/>
+                      onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist}/>
           </div>
         </div>
     </div>
